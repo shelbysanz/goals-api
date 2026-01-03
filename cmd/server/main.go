c48fef8f048bb4ec11957691acb13eea8d65795a
shelbysanz <shelbsgt500@gmail.com>
2026-01-03 18:11:12 -0500

    build: adding auto migrate before api startup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	"goals-api/internal/db"
+	"goals-api/internal/models"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,6 +16,22 @@ func main() {
 	e.GET("/health", func(c echo.Context) error {
 		return c.String(http.StatusOK, "healthy")
 	})
+
+	database, err := db.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Running migrations...")
+	err = database.AutoMigrate(
+		&models.MonthGoal{},
+		&models.WeekGoal{},
+		&models.WeekTodo{},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Migrations complete")
 
 	e.Start(":8080")
 }
